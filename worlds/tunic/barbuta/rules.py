c529878f03dcdde03ced4d1a066904495db42327
Scipio Wright <scipiowright@gmail.com>
2024-10-08 19:50:11 -0400

    Reworking to not be by room

--- worlds/tunic/barbuta/rules.py
+++ worlds/tunic/barbuta/rules.py
@@ -1,19 +1,17 @@
 from typing import TYPE_CHECKING, Dict
-from BaseClasses import Region
+from BaseClasses import Region, CollectionState
 
 if TYPE_CHECKING:
     from .. import TunicWorld
 
 
 def create_barbuta_rules(world: "TunicWorld", regions: Dict[str, Region]) -> None:
-    # D4
-    regions["D4"].connect(connecting_region=regions["D5"])
-
-    regions["D4 Upper Right"].connect(connecting_region=regions["D5 Upper"])
-
-    # D5, the starting room
-    regions["D5"].connect(connecting_region=regions["D4"])
-    regions["D5"].connect(connecting_region=regions["D6"])
-
-    regions["D5 Upper"].connect(connecting_region=regions["D4 Upper Right"])
-    regions["D5 Upper"].connect(connecting_region=regions["D6 Upper Left"])
+    player = world.player
+    regions["Starting Area"].connect(regions["Key Room"])
+    regions["Starting Area"].connect(regions["Platforms above D4"], rule=lambda state: state.has("Necklace", player))
+    regions["Starting Area"].connect(regions["Blood Sword Room"]),
+    # pin via G2, umbrella via H3
+    regions["Starting Area"].connect(regions["G3 and Nearby"],
+                                     rule=lambda state: state.has_any(("Pin", "Umbrella"), player))
+    regions["Starting Area"].connect(regions["F7 and Nearby"],
+                                     rule=lambda state: state.has("Pin", player)),
