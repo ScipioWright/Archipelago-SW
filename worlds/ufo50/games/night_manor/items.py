e7b031f5f87df79a51bd24620615d632dc51634d
Scipio Wright <scipiowright@gmail.com>
2024-11-15 09:23:15 -0500

    Add world to get_filler_item_name

--- worlds/ufo50/games/night_manor/items.py
+++ worlds/ufo50/games/night_manor/items.py
@@ -153,5 +153,5 @@ def create_items(world: "UFO50World") -> List[Item]:
     return night_manor_items
 
 
-def get_filler_item_name() -> str:
+def get_filler_item_name(world: "UFO50World") -> str:
     return "Night Manor - Yellow Note"
