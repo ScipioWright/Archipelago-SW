cfe5bc45140c7b842b57f68a94a1bf4c7ce29dad
Scipio Wright <scipiowright@gmail.com>
2025-06-14 16:55:08 -0400

    Update version

--- worlds/animal_well/__init__.py
+++ worlds/animal_well/__init__.py
@@ -77,7 +77,7 @@ class AnimalWellWorld(World):
     """
     game = "ANIMAL WELL"
     web = AnimalWellWeb()
-    version_string: str = "v0.6.0 RC2"
+    version_string: str = "v0.6.0"
 
     options: AnimalWellOptions
     options_dataclass = AnimalWellOptions
